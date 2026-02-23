d6102cd939a2d8812b9974e9d7849b9baf0c0522
Manjot Kaur <manjotkaur311205@gmail.com>
2026-02-24 01:54:54 +0530

    Update menu bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
 document.querySelector("button").addEventListener("click", () => {
     window.print();
 })
+
+var menuBar = document.querySelector(".menu-bar");
+var menuSection = document.querySelector(".menu-section");
+menuBar.addEventListener("click", () => {
+    menuBar.style.display = "none";
+    menuSection.style.display = "block";
+})
+
+
+var closeButton = document.querySelector(".close-button")
+var count = 0;
+
+closeButton.addEventListener("click",() => {
+    if(count%2==0){
+        closeButton.style.display = "block";
+        menuSection.style.display = "block";
+        menuBar.style.display = "none";
+    } else {
+        menuBar.style.display = "block";
+        closeButton.style.display = "block";
+        menuSection.style.display = "none";
+    }
+    count++;
+});
